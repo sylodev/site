52c7b8880c2666ed3c818124f0f11e9e7dd62d65
Ryan <ryan@sylver.me>
2021-06-07 09:41:42 +0800

    a11y/lighthouse

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,7 +12,7 @@ export const Card: React.FC<CardProps> = (props) => {
     <Link href={props.link} passHref>
       <a className="flex flex-col justify-between text-left border border-transparent rounded hover:shadow-xl bg-box hover:border-border">
         <div className="p-4">
-          <h5 className="text-xl font-bold text-blue-500">{props.title}</h5>
+          <p className="text-xl font-bold text-blue-500">{props.title}</p>
           <p className="pt-2">{props.description}</p>
         </div>
         <div className="p-4 text-sm bg-black bg-opacity-60 text-light">{props.footer}</div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 export default class CustomDocument extends Document {
   public render() {
     return (
-      <Html>
+      <Html lang="en-US">
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
         </Head>
